f5d2d1518f202f63947adf9d7763c99a869731bb
Deepan <deepan.ppgit@gmail.com>
2022-09-28 21:15:56 +0530

    reduced quiz length

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,5 +3,5 @@ class Quiz < ApplicationRecord
 	has_many :quiz_categories
 	has_many :submissions, :dependent => :destroy
   has_many :categories, through: :quiz_categories
-	validates :name, uniqueness: true, presence: true, length: {minimum: 3, maximum: 30}, allow_blank: false
+	validates :name, uniqueness: true, presence: true, length: {minimum: 3}, allow_blank: false
 end
