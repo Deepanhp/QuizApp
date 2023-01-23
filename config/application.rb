f220acd11380ff665a1319db41395df60f2f8944
saravana <43269754+saravana2865@users.noreply.github.com>
2023-01-24 02:01:26 +0530

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module QuizApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.autoload_paths += %W(#{config.root}/lib)
-    config.time_zone = 'kolkata'
+    config.time_zone = 'Kolkata'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
