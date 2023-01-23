c508fb57c49ac2449c433219d1539421e886a1af
saravana <43269754+saravana2865@users.noreply.github.com>
2023-01-24 01:27:07 +0530

    Update 20230123163747_add_columns.rb

--- db/migrate/20230123163747_add_columns.rb
+++ db/migrate/20230123163747_add_columns.rb
@@ -1,4 +1,4 @@
-class AddColumns < ActiveRecord::Migration[7.0]
+class AddColumns < ActiveRecord::Migration[5.1]
     def change
       add_column :submissions, :submitted, :boolean
       add_column :quizzes, :duration, :integer
